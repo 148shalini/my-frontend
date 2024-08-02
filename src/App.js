a51c4d3ec171c275218b38d476839c8de4356123
rohandass58 <rohandass58@gmail.com>
2024-08-02 13:06:18 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
 import ReceiveMessage from './components/ReceiveMessage';
 import Register from './components/Register';
 import UserList from './components/UserList';
 
-
-
-
 const App = () => {
-  const token = localStorage.getItem('accessToken');
+  const [token, setToken] = useState(localStorage.getItem('accessToken'));
 
   const handleLogout = () => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
-    window.location.href = '/login';
+    setToken(null); // Update state to reflect logout
   };
 
   return (
@@ -23,11 +21,11 @@ const App = () => {
       <div className="app">
         <Routes>
           <Route path="/" element={<PrivateRoute element={<Navigate to="/user-list" />} />} />
-          <Route path="/login" element={!token ? <Login /> : <Navigate to="/user-list" />} />
-          <Route path="/register" element={!token ? <Register /> : <Navigate to="/user-list" />} />
+          <Route path="/login" element={!token ? <Login setToken={setToken} /> : <Navigate to="/user-list" />} />
+          <Route path="/register" element={!token ? <Register setToken={setToken} /> : <Navigate to="/user-list" />} />
           <Route path="/user-list" element={<PrivateRoute element={<UserList />} />} />
           <Route path="/receive-messages" element={<PrivateRoute element={<ReceiveMessage />} />} />
-
+          {/* Add other routes here */}
         </Routes>
         {token && <button onClick={handleLogout}>Logout</button>}
       </div>
