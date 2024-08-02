0e8d266e27801404bd91f1a5cb9323350fb58f1f
rohandass58 <rohandass58@gmail.com>
2024-08-02 09:37:41 +0530

    updated teh design and included the userlist part

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
@@ -6,21 +6,30 @@ import ReceiveMessage from './components/ReceiveMessage';
 import Register from './components/Register';
 import UserList from './components/UserList';
 
+
+
+
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem('accessToken'));
+  const token = localStorage.getItem('accessToken');
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    window.location.href = '/login';
+  };
 
   return (
     <Router>
       <div className="app">
         <Routes>
           <Route path="/" element={<PrivateRoute element={<Navigate to="/user-list" />} />} />
-          <Route path="/login" element={token ? <Navigate to="/user-list" /> : <Login setToken={setToken} />} />
-          <Route path="/register" element={token ? <Navigate to="/user-list" /> : <Register />} />
+          <Route path="/login" element={!token ? <Login /> : <Navigate to="/user-list" />} />
+          <Route path="/register" element={!token ? <Register /> : <Navigate to="/user-list" />} />
           <Route path="/user-list" element={<PrivateRoute element={<UserList />} />} />
           <Route path="/receive-messages" element={<PrivateRoute element={<ReceiveMessage />} />} />
-          {/* Add other routes here */}
+
         </Routes>
-        {token && <button onClick={() => { localStorage.removeItem('accessToken'); localStorage.removeItem('refreshToken'); setToken(null); }}>Logout</button>}
+        {token && <button onClick={handleLogout}>Logout</button>}
       </div>
     </Router>
   );
